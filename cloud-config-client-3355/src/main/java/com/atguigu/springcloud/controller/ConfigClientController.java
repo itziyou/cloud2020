8ebfe0f5550a1cd5ba7a2471b986d7dcc8cf4028
ziyou <itziyou@aliyun.com>
2020-05-09 18:10:39 +0800

    SpringCloud Config学习完结

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: zy
+ * @DateTime: 2020/5/9 15:44
+ * @Description:
+ */
+@RefreshScope
+@RestController
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
