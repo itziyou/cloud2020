adb7dd5f9e5edfeb84200f480695a5012c4b3b05
czx <bears631@163.com>
2020-05-13 23:18:52 +0800

    SpringCloud Alibaba Nacos学习

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/alibaba/OrderNacosMain83.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zy
+ * @Description
+ * @date 2020-05-13 23:07
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/alibaba/PaymentMain9002.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zy
+ * @Description
+ * @date 2020-05-13 21:56
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
