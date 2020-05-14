08696e6006c0d5c90afdee8af4f12e7327193c54
czx <bears631@163.com>
2020-05-14 22:41:39 +0800

    SpringCloud Alibaba Nacos学习Namespace、Group、DataID

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zy
+ * @Description
+ * @date 2020-05-14 21:24
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
