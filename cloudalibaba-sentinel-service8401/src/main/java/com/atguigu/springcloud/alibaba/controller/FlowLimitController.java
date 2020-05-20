e632e5c9d2dc58ed914d76bf40f09cd5bba77376
ziyou <itziyou@aliyun.com>
2020-05-20 19:40:44 +0800

    SpringCloud Alibaba Sentinel学习

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: zy
+ * @DateTime: 2020/5/20 19:14
+ * @Description:
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "------testB";
+    }
+}
