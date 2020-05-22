732561e611f4b2ed8ea9ae85ee7bf1337732a464
ziyou <itziyou@aliyun.com>
2020-05-22 16:48:43 +0800

    SpringCloud Alibaba Sentinel流控降级

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Author: zy
  * @DateTime: 2020/5/20 19:14
@@ -19,7 +21,33 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB() {
-        log.info("{},....testB",Thread.currentThread().getName());
+        log.info("{},....testB", Thread.currentThread().getName());
         return "------testB";
     }
+
+
+    @GetMapping("/testD")
+    public String testD() {
+/*        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");*/
+
+        log.info("testD 异常比例");
+        int age = 10 / 0;
+
+        return "------testD";
+    }
+
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试异常数");
+        int age = 10 / 0;
+        return "------testE 测试异常数";
+    }
+
+
 }
