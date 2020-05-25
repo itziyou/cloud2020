fc6c8da0ddab0ce594fb2de3837cd4cf04c5303f
ziyou <itziyou@aliyun.com>
2020-05-25 15:02:14 +0800

    SpringCloud Alibaba @SentinelResource学习

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/myhandler/CustomerBlockHandler.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.alibaba.myhandler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.entitis.CommonResult;
+
+/**
+ * @Author: zy
+ * @DateTime: 2020/5/25 10:55
+ * @Description:
+ */
+public class CustomerBlockHandler {
+    public static CommonResult handlerException(BlockException exception) {
+        return new CommonResult(4444, "按客戶自定义,global handlerException----1");
+    }
+
+    public static CommonResult handlerException2(BlockException exception) {
+        return new CommonResult(4444, "按客戶自定义,global handlerException----2");
+    }
+}
